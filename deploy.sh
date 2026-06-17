88a3c5b152769550aeef6e0442eaaf4040eb0976
Robin Chew <me@robin.com.au>
2026-06-18 00:43:27 +0800

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-rsync -avu --exclude={'.git','run.sh','.erlang.mk','_rel','deps','ebin'} ./ 192.168.1.69:webgate
+rsync -avu --exclude={'.git','run.sh','.erlang.mk','_rel','deps','ebin'} ./ 192.168.0.6:webgate
